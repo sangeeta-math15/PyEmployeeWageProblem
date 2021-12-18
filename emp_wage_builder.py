a114c8c325ca776267353312661c2ac30bb11d2e
sngtmth <sngtmth@gmail.com>
2021-12-18 23:34:45 +0530

    Added:Calculate daily wages

--- emp_wage_builder.py
+++ emp_wage_builder.py
@@ -1,26 +1,38 @@
 import logging
 import random
 
-logging.basicConfig(filename='emp_wage.log',filemode='w')
-log=logging.getLogger()
+
+logging.basicConfig(filename='emp_wage.log', filemode='w')
+log = logging.getLogger()
 log.setLevel(logging.DEBUG)
 
-#Create employeeWageBuilder class
+
 class EmpWageBuilder:
+
     def __init__(self):
         self.check_attendence=0
+
     def emp_attendence(self):
-        check_attendence=random.randint(0,1)
-        if check_attendence == 0:
+        is_full_time = 1
+        check_attendence = random.randint(0, 1)
+        if check_attendence == is_full_time:
             log.info("Employee is present")
-            return "Employee is present"
+            emp_hr = 8
+            return emp_hr
         else:
-            return "Employee is absent"
+            log.info("Employee is absent")
+            emp_hr = 0
+            return emp_hr
+
+    # Create employeeWageBuilder class
+    def cal_daily_wage(self, emp_hr):
+        emp_wage_per_hour = 20
+        emp_wage = emp_hr * emp_wage_per_hour
+        return emp_wage
+
 
 if __name__ == '__main__':
-    emp=EmpWageBuilder()
-    print(emp.emp_attendence())
-
-
-
-
+    emp = EmpWageBuilder()
+    emp_hr = emp.emp_attendence()
+    daily_wage = emp.cal_daily_wage(emp_hr)
+    print(daily_wage)
--- emp_wage_test.py
+++ emp_wage_test.py
@@ -1,7 +1,11 @@
 from emp_wage_builder import EmpWageBuilder
 import pytest
-def test_check_attendance():
-    emp = EmpWageBuilder()
-    assert emp.emp_attendence() == "Employee is present"
 
+emp = EmpWageBuilder()
+def test_check_attendance():
+    assert (emp.emp_attendence() == 8 or emp.emp_attendence() == 0)
+
+def test_daily_wage():
+    assert cal_daily_wage(8) == 160
+    assert cal_daily_wage(0) == 0
 
