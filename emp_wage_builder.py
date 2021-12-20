eeba15330a346527dd8bf65ab9c592d3d58c0319
sngtmth <sngtmth@gmail.com>
2021-12-21 01:19:49 +0530

    Added:conditional wages

--- emp_wage_builder.py
+++ emp_wage_builder.py
@@ -8,20 +8,27 @@ log.setLevel(logging.DEBUG)
 
 class EmpWageBuilder:
 
-    def __init__(self, emp_hr=0, emp_wage=0, total_emp_wage=0):
+    def __init__(self, emp_hr=0, emp_wage=0, total_emp_wage=0, total_working_days=0, total_working_hours=0):
         self.emp_hr = emp_hr
         self.emp_wage = emp_wage
         self.total_emp_wage = total_emp_wage
+        self.total_working_days = total_working_days
+        self.total_working_hours = total_working_hours
 
     def total_employee_wage(self):
         no_working_day = 20
         emp_rate_per_hr = 20
-        for day in range(1, no_working_day):
+        max_hrs_in_month = 100
+        while self.total_working_hours <= max_hrs_in_month and self.total_working_days <= no_working_day:
+            self.total_working_days += 1
             emp_check = random.randint(1, 3)
             self.emp_hr = self.get_work_hours(emp_check)
+            self.total_working_hours = self.total_working_hours + self.emp_hr
+            print("totalworking hour", self.total_working_hours)
+            print("Days: {} Emp Hr: {}".format(self.total_working_days, self.emp_hr))
             self.emp_wage = self.emp_hr * emp_rate_per_hr
             self.total_emp_wage = self.total_emp_wage + self.emp_wage
-            print("Total Monthly Wages:", self.total_emp_wage)
+            print("Total conditional wage:", self.total_emp_wage)
 
     def get_work_hours(self, value):
         switcher = {
